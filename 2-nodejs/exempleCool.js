00c60710cc4daa8bcf5b9e04a9542876ee0da5fe
Didinozor <diego-napolitano@hotmail.com>
2024-03-27 16:22:20 +0100

    kys

---
+++ 2-nodejs/exempleCool.js
@@ -0,0 +1,9 @@
+$(function(){
+    console.log('Ready for ma sho');
+
+    $('#cmdJumbo').click(function (e) { 
+        e.preventDefault();
+        alert('Kill yourself :|')
+    });
+
+});
---
+++ 2-nodejs/exempleFat.js
@@ -0,0 +1,7 @@
+console.log('Exemple d\'utilisation d\'un timer')
+
+setTimeout(()=>{
+    console.log('Cououuuuuuuuuuuuuuuuuu');
+},5000)
+
+console.log('Le timer s\'est écoulé')
